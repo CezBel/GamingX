a96f20fe67071a2e88ade76cabc94cded030f972
cezar <Tzezar32@gmail.com>
2023-12-12 09:27:47 +0200

    starting_redux

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fatchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    
+    fatchProducts();
+  }, []);
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+export default HomeScreen;
